58f2fc8fdc2f3e6e4343daa0a0b7257f09833178
Kerrick Long <me@kerricklong.com>
2026-06-27 01:02:58 -0500

    Initial Commit

---
+++ EbookDigitizer/Sources/EbookDigitizerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import EbookDigitizerCore
+
+@main
+struct EbookDigitizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Canvas Ready")
+                .frame(width: 800, height: 600)
+        }
+    }
+}
---
+++ EbookDigitizerCore/Sources/VisionProcessingActor.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Vision
+
+public actor VisionProcessingActor {
+    public init() {}
+    
+    public func processImage() {
+        // Phase 2: Vision framework logic goes here
+    }
+}
